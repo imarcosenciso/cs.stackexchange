d1b21375b751f3a1b12b296071a84d43902b8015
imarcosenciso <48628072+imarcosenciso@users.noreply.github.com>
2021-12-18 19:26:05 +0100

    minor changes in scrape

--- Data/scrape.py
+++ Data/scrape.py
@@ -2,23 +2,23 @@
 
 This scraper role is to recieve a tag and return its
 description, using Wikipedia's API: MediaWiki API.
+
+    Functions
+    -------
+    get_description(tag: str, s: requests.Session) -> str
+        Recieves a tag as parameter and returns the description of the tag obtained from the API.
+        Parameters
+        ----------
+        tag: str
+            Tag to be searched.
+        session: requests.Session
+            Session to be passed in the main script.
 """
 import requests
 
-
-test_tags = ["Algorithm",
-             "Big O notation",
-             "Turing Complete",
-             "cpu-pipelines",
-             "computer-architecture",
-             "database-theory",
-             "average-case",
-             "time-complexity"
-             ]
-
 URL = "https://en.wikipedia.org/w/api.php"  # Engish Wikipedia API endpoint
 
-"""A lot of tunning has gone into it and they return good results for the most part. Better not touch."""
+# A lot of tunning has gone into it and they return good results for the most part. Better not touch.
 PARAMS = {
     "action": "query",
     "generator": "prefixsearch",
@@ -33,9 +33,17 @@ PARAMS = {
 }
 
 
-def get_description(tag: str) -> str:
-    PARAMS["gpssearch"] = tag
-    data = s.get(url=URL, params=PARAMS).json()
+def get_description(tag: str, session: requests.Session, url: str = URL, params: dict = PARAMS) -> str:
+    """Returns description of a tag
+    Parameters
+    ----------
+    tag: str
+        Tag to be searched.
+    session: requests.Session
+        Session to be passed in the main script.
+    """
+    params["gpssearch"] = tag
+    data = session.get(url=url, params=params).json()
 
     # Extract unknown key (each query returns a random number as key).
     key = list(data["query"]["pages"].keys())[0]
@@ -44,9 +52,19 @@ def get_description(tag: str) -> str:
 
 
 if __name__ == "__main__":
-    s = requests.Session()
+    test_tags = ["Algorithm",
+                 "Big O notation",
+                 "Turing Complete",
+                 "cpu-pipelines",
+                 "computer-architecture",
+                 "database-theory",
+                 "average-case",
+                 "time-complexity"
+                 ]
+
+    S = requests.Session()
 
     for t in test_tags:
-        desc = get_description(t)
+        desc = get_description(t, S)
         print(
             f"Tag: {t}.\nDescription: {desc}\n == == == == == == == == == == == == == == ==\n")
